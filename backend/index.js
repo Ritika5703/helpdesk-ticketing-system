32b739be26d901a28ec740570c18766ad4ae2b18
Ritika Mishra <mishraritika579@gmail.com>
2025-07-03 22:21:58 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,18 @@ const allowedOrigins = [process.env.FRONTEND_URL];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // This ensures URL-encoded body parsing
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // API Endpoint
 app.get("/", (req, res) => res.send("API Working"));
