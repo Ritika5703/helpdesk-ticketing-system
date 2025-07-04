3cca360e29b48d5e6188fd9af4eee952282652c2
Ritika Mishra <mishraritika579@gmail.com>
2025-07-04 21:02:07 +0530

    included multer

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createTicket, getMyTickets } from "../controllers/ticketController.js";
-
+import multer from "multer";
 import userAuth from "../middleware/userAuth.js";
 
 const router = express.Router();
-
-router.post("/create", userAuth, createTicket); // POST /api/tickets
+const upload = multer({ storage: multer.memoryStorage() });
+router.post("/create", userAuth, upload.single("attachment"), createTicket); // POST /api/tickets
 router.get("/my", userAuth, getMyTickets); // GET /api/tickets/my
 
 export default router;
